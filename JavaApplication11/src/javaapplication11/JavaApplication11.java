abc1ae37b9a8f258be454177183765b32741a1a1
jvaldezm1 <jvaldezm1@miumg.edu.gt>
2023-06-03 07:38:23 -0600

    practica swing java

---
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -0,0 +1,92 @@
+
+package javaapplication11;
+
+import java.util.Scanner;
+
+
+public class JavaApplication11 {
+
+   
+    public static void main(String[] args) {
+          Scanner sn= new Scanner (System.in);
+        Calculadora Calculadora = new Calculadora();
+     calculadoraEspecial CalculadoraEspecial=new calculadoraEspecial();
+        boolean salir = false;
+        int opcion;
+        
+        
+        do {
+        
+         System.out.println("");
+           System.out.println("=======================");
+           System.out.println("1. Sumar");
+           System.out.println("2. Restar");
+           System.out.println("3. Multiplicar");
+           System.out.println("4. Dividir");
+           System.out.println("5. Raiz Cuadrada");
+           System.out.println("6. Verificar numero Primo");
+           System.out.println("********");
+           System.out.println("Calculadora Especial");
+             System.out.println("********");
+           System.out.println("7. Radio de un circulo");
+           System.out.println ("8. calculo de un factorial");
+           System.out.println("9. comun divisor");
+           System.out.println("10. Generar numero aleatorio");
+           System.out.println("11. calculo de un numero decimal");
+           System.out.println("12. Calcular area de un cuadrado");
+           System.out.println("Selecciona una opcion:  ");
+           System.out.println("=======================");
+           opcion = sn.nextInt();
+          
+           
+           
+          switch (opcion){
+               
+               case 1:System.out.println("El resultado de la suma es = " + Calculadora.sumar(10, 5));
+                   break;
+               case 2: 
+                System.out.println("El resultado de la resta es = " +Calculadora.restar(10, 5));
+                   break;
+               case 3:
+                   System.out.println("El resultado de la multiplicacion es = " +Calculadora.multiplicar(10, 5));
+                   break;
+               case 4:
+                   System.out.println("El resultado de la division es = " +Calculadora.dividir(10, 5));
+                   break;
+                   
+               case 5:
+                  System.out.println("El resultado de la raiz cuadrada es = " +Calculadora.raizCuadrada(16));
+                   break;
+                   
+               case 6:
+                  System.out.println("Es primo? = " +Calculadora.esPrimo(11));
+                   break;
+                   
+               case 7:
+             System.out.println("el radio es  = " +calculadoraEspecial.calcularAreaCirculo(25));
+                   break;
+                   
+               case 8: 
+                   System.out.println("El calculo del factorial es  = " +calculadoraEspecial.calcularFactorial (10));
+                   break;
+                   
+               case 9:
+                   System.out.println("El comun divisor es de= " +calculadoraEspecial.calcularMCD(6,9));
+                   break;
+               case 10:
+                   System.out.println("El numero aleatorio es = " +calculadoraEspecial.generarNumeroAleatorio(50,70));
+                   break;
+               case 11:
+                   System.out.println("El numero decimal es = " +calculadoraEspecial.redondearDecimal(15,25));
+                   break;
+               case 12:
+                 System.out.println("area de un cuadrado es = "+ calculadoraEspecial.CalculoArea(5,5));
+                   break;
+           }
+           
+        }  while(!salir);  
+      
+    }
+    }
+    
+
---
+++ JavaApplication11/src/javaapplication11/calculadoraEspecial.java
@@ -0,0 +1,45 @@
+
+package javaapplication11;
+
+
+public class calculadoraEspecial {
+     //Are de declaración de metodos
+    //en clase hemos dicho "funciones" anteriormente, pero en POO son metodos.
+    
+     
+//calculo de area de un circulo  
+ public static double calcularAreaCirculo(double radio) {
+    return Math.PI * Math.pow(radio, 2);
+}
+ 
+ //calculo de un factorial
+   public static int calcularFactorial(int numero) {
+    if (numero == 0)
+        return 1;
+    else
+        return numero * calcularFactorial(numero - 1);
+}
+   
+  //calcular el comun divisor 
+   public static int calcularMCD(int num1, int num2) {
+    if (num2 == 0)
+        return num1;
+    return calcularMCD(num2, num1 % num2);
+}
+   
+   //generar numero aleatorio
+  public static int generarNumeroAleatorio(int min, int max) {
+    return min + (int) (Math.random() * ((max - min) + 1));
+}
+    
+  //calculo para redondear un decimal
+  public static double redondearDecimal(double numero, int decimales) {
+    int factor = (int) Math.pow(10, decimales);
+    return Math.round(numero * factor) / (double) factor;
+}
+  
+  //calcular area de un cuadrado 
+ public static int  CalculoArea( int numero1 ,int numero2  ){
+     return numero1 * numero2;
+ }
+}
